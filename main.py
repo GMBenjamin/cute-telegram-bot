d68cc49a0c5d983992bee92c71c1ebb0dbe3342c
unknown <benjamin.gaete@ug.uchile.cl>
2024-11-15 23:38:04 -0300

    Update: Use of images from internet

--- main.py
+++ main.py
@@ -8,25 +8,64 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+class Picture:
+    def __init__(self, link, caption):
+        self.picture = link
+        self.text = "**Source:** " + caption
+    def get_picture(self):
+        return self.picture
+    def get_caption(self):
+        return self.text
+
+cats = []
+c=Picture("https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg",
+              "Pixabay on Pexels")
+cats.append(c)
+c=Picture("https://images.pexels.com/photos/96938/pexels-photo-96938.jpeg",
+              "Francesco Ungaro on Pexels")
+cats.append(c)
+c=Picture("https://images.pexels.com/photos/735423/pexels-photo-735423.jpeg",
+              "Eftodii Aurelia on Pexels")
+cats.append(c)
+c=Picture("https://images.pexels.com/photos/1317844/pexels-photo-1317844.jpeg",
+            "Guillaume Meurice on Pexels")
+cats.append(c)
+
+hedgehogs = []
+h=Picture("https://images.pexels.com/photos/50577/hedgehog-animal-baby-cute-50577.jpeg",
+          "Pixabay on Pexels")
+hedgehogs.append(h)
+h=Picture("https://images.pexels.com/photos/20080177/pexels-photo-20080177/free-photo-of-manos-mujer-primavera-flores.jpeg",
+          "Marina Riijik on Pexels")
+hedgehogs.append(h)
+h=Picture("https://images.pexels.com/photos/207028/pexels-photo-207028.jpeg",
+          "Pixabay on Pexels")
+hedgehogs.append(h)
+h=Picture("https://images.pexels.com/photos/209246/pexels-photo-209246.jpeg",
+          "Pixabay on Pexels")
+hedgehogs.append(h)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text="Let's begin!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=
+                                   "Let's begin!")
 
 async def hi(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text="Hello there!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=
+                                   "Hello there!")
 
 async def cat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    photo_id = str(random.randint(0, 3))
-    path = "Cats/"+photo_id+".jpg"
+    pic = cats[random.randint(0, len(cats) - 1)]
     await context.bot.send_photo(chat_id=update.effective_chat.id,
-                                 photo=open(path, "rb"))
+                                 photo=pic.get_picture(),
+                                 caption=pic.get_caption(),
+                                 parse_mode="Markdown")
 
 async def hedgehog(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    photo_id = str(random.randint(0, 3))
-    path = "Hedgehogs/"+photo_id+".jpg"
+    pic = hedgehogs[random.randint(0, len(hedgehogs) - 1)]
     await context.bot.send_photo(chat_id=update.effective_chat.id,
-                                 photo=open(path, "rb"))
+                                 photo=pic.get_picture(),
+                                 caption=pic.get_caption(),
+                                 parse_mode="Markdown")
 
 if __name__ == '__main__':
     TokenBot = "TOKEN"
